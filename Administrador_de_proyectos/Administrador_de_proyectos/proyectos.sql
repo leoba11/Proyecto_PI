6d2b83bd94a089ea4e9f35845b19d835404c1311
Johnn Enderson Hernandez Delgado <johnenderhernandez@gmail.com>
2019-09-16 12:43:18 -0600

    cambiando formato del costo estimado

--- Administrador_de_proyectos/Administrador_de_proyectos/proyectos.sql
+++ Administrador_de_proyectos/Administrador_de_proyectos/proyectos.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[proyectos]
 (
 	[codigoPK] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [nombre] NCHAR(10) NULL, 
-    [fechaInicio] DATETIME NULL, 
-    [fechaEstimada] DATETIME NULL, 
-    [costoEstimado] NCHAR(10) NOT NULL,  /*calcular*/
-    [objetivo] NCHAR(10) NOT NULL, 
+    [nombre] VARCHAR(50) NOT NULL, 
+    [fechaInicio] DATETIME NOT NULL, 
+    [fechaEstimada] DATETIME NOT NULL, 
+    [costoEstimado] MONEY NOT NULL,  /*calcular con base en fechas*/
+    [objetivo] VARCHAR(MAX) NOT NULL, 
     [cedulaClienteFK] INT NOT NULL, 
     [idEquipo] VARCHAR(50) NULL
 )
