b29e1bb2bafd1a327c7aac28a99724c16ac582b9
John Hernández <johnenderhernandez@gmail.com>
2019-10-02 18:24:20 -0600

    agregando equipos a layout

---
+++ Proy_PI/Proy_PI/Controllers/equiposController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proy_PI.Controllers
+{
+    public class equiposController : Controller
+    {
+        // GET: equipos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
