4b76cd191b11e1a6ff30f9a796347ef1ea6594cb
John Hernández <johnenderhernandez@gmail.com>
2019-10-21 10:42:47 -0600

    Dropdownlist inicial para los reportes

--- Proyecto_Integrador/Proyecto_Integrador/Controllers/reportesController.cs
+++ Proyecto_Integrador/Proyecto_Integrador/Controllers/reportesController.cs
@@ -9,8 +9,59 @@ namespace Proyecto_Integrador.Controllers
     public class reportesController : Controller
     {
         // GET: reportes
+        [HttpGet]
         public ActionResult Index()
         {
+            List<SelectListItem> mySkills = new List<SelectListItem>() {
+                new SelectListItem {
+                    Text = "opcion 1", Value = "1"
+                },
+                new SelectListItem {
+                    Text = "ASP.NET WEB API", Value = "2"
+                },
+                new SelectListItem {
+                    Text = "ENTITY FRAMEWORK", Value = "3"
+                },
+                new SelectListItem {
+                    Text = "DOCUSIGN", Value = "4"
+                },
+                new SelectListItem {
+                    Text = "ORCHARD CMS", Value = "5"
+                },
+                new SelectListItem {
+                    Text = "JQUERY", Value = "6"
+                },
+                new SelectListItem {
+                    Text = "ZENDESK", Value = "7"
+                },
+                new SelectListItem {
+                    Text = "LINQ", Value = "8"
+                },
+                new SelectListItem {
+                    Text = "C#", Value = "9"
+                },
+                new SelectListItem {
+                    Text = "GOOGLE ANALYTICS", Value = "10"
+                },
+            };
+            TempData["reportes"] = mySkills;
+            TempData.Keep();
+            return RedirectToAction("SelectReport", "reportes");
+        }
+
+        public ActionResult SelectReport()
+        {
+            TempData.Keep();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SelectReport(string reporte)
+        {
+            TempData.Keep();
+
+
+            /*aqui se agrega if, para redirigir del reporte correspondiente*/
             return View();
         }
     }
