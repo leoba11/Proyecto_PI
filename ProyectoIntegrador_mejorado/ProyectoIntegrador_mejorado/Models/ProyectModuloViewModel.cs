5a8a35c04a9ac6f1788284052c6a15712d1b3593
Maria Jose Aguilar Barboza <1430mjab@gmail.com>
2019-11-03 21:42:24 -0600

    requerimientos completo para demo

---
+++ ProyectoIntegrador_mejorado/ProyectoIntegrador_mejorado/Models/ProyectModuloViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoIntegrador_mejorado.Models
+{
+    public class ProyectModuloViewModel
+    {
+        public int codigoPK { get; set; }
+
+        public int idPK { get; set; }
+    }
+}
